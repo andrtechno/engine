f28337cb693a24dfb0c963ecf0d03e2c881f484e
andrtechno <andrew.panix@gmail.com>
2017-10-15 14:02:38 +0300

    init

--- messages/ru/admin.php
+++ messages/ru/admin.php
@@ -1,10 +1,9 @@
 <?php
 
 return [
-
     'LOGIN_ADMIN_PANEL' => 'Войти в панель управление',
     'CMS' => 'Система',
     'ADMIN_PANEL' => 'Админ-панель',
-    'SORT_SUCCESS_MESSAGE'=>'Элементы успешно отсортированы'
+    'SORT_SUCCESS_MESSAGE' => 'Элементы успешно отсортированы',
+    'EDIT_GRID_TABLE' => 'Изменить столбцы таблицы'
 ];
-
