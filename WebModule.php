26d18094a2afdd234cda931d00b1a31e1e35961a
PAN-ПК <andrew.panix@gmail.com>
2017-11-28 10:09:35 +0200

    init

--- WebModule.php
+++ WebModule.php
@@ -7,7 +7,9 @@ use yii\base\Module;
 use yii\helpers\FileHelper;
 
 class WebModule extends Module {
-  // protected $_icon;
+
+    public $assetsUrl;
+    // protected $_icon;
     public $count = false;
     // protected $info;
     public $routes = [];
@@ -17,6 +19,7 @@ class WebModule extends Module {
     public $icon;
     public $uploadPath;
     public $uploadAliasPath = null;
+
     public function getMdFiles() {
         $list = [];
         $files = FileHelper::findFiles(Yii::getAlias('@' . $this->id), [
@@ -32,22 +35,27 @@ class WebModule extends Module {
 
     // protected $moduleNamespace;
     public function init() {
+        if (file_exists(Yii::getAlias("@{$this->id}/assets"))) {
+            $assetsPaths = Yii::$app->getAssetManager()->publish(Yii::getAlias("@{$this->id}/assets"));
+            $this->assetsUrl = $assetsPaths[1];
+        }
+
         //$this->registerTranslations();
-        
+
         $this->uploadAliasPath = "@webroot/uploads/content/{$this->id}";
         $this->uploadPath = "/uploads/content/{$this->id}";
-        
+
         if (method_exists($this, 'getDefaultModelClasses')) {
             $this->modelClasses = array_merge($this->getDefaultModelClasses(), $this->modelClasses);
         }
         parent::init();
     }
-    
-        public function afterInstall() {
-       // if ($this->uploadAliasPath && !file_exists(Yii::getPathOfAlias($this->uploadAliasPath)))
-       //     CFileHelper::createDirectory(Yii::getPathOfAlias($this->uploadAliasPath), 0777);
+
+    public function afterInstall() {
+        // if ($this->uploadAliasPath && !file_exists(Yii::getPathOfAlias($this->uploadAliasPath)))
+        //     CFileHelper::createDirectory(Yii::getPathOfAlias($this->uploadAliasPath), 0777);
         //Yii::$app->cache->flush();
-       // Yii::app()->widgets->clear();
+        // Yii::app()->widgets->clear();
         return true;
     }
 
@@ -57,24 +65,20 @@ class WebModule extends Module {
     public function afterUninstall() {
         //if ($this->uploadAliasPath && !file_exists(Yii::getPathOfAlias($this->uploadAliasPath)))
         //    CFileHelper::removeDirectory(Yii::getPathOfAlias($this->uploadAliasPath), array('traverseSymlinks' => true));
-
         //if (file_exists(Yii::getPathOfAlias("webroot.uploads.attachments.{$this->id}")))
         //    CFileHelper::removeDirectory(Yii::getPathOfAlias("webroot.uploads.attachments.{$this->id}"), array('traverseSymlinks' => true));
-
-       // Yii::$app->cache->flush();
-
-
+        // Yii::$app->cache->flush();
         //$moduleName = ucfirst($this->id) . '.';
 
 
         return true;
     }
-    
-   // public function getIcon() {
-    //    return $this->_icon;
-   // }
 
-   public function setIcon($icon) {
+    // public function getIcon() {
+    //    return $this->_icon;
+    // }
+
+    public function setIcon($icon) {
         $this->icon = $icon;
     }
 
@@ -83,10 +87,11 @@ class WebModule extends Module {
     }
 
     public function getName() {
-        return Yii::t($this->id."/default", 'MODULE_NAME');
+        return Yii::t($this->id . "/default", 'MODULE_NAME');
     }
 
     public function getDescription() {
-        return Yii::t($this->id."/default", 'MODULE_DESC');
+        return Yii::t($this->id . "/default", 'MODULE_DESC');
     }
+
 }
---
+++ components/BrowserComponent.php
@@ -0,0 +1,67 @@
+<?php
+namespace panix\engine\components;
+/**
+ * BrowserComponent class
+ * 
+ * @author CORNER CMS development team <dev@corner-cms.com>
+ * @package app
+ * @subpackage addons
+ * @uses CApplicationComponent
+ * @copyright (c) 2016, Andrew Semenov
+ * @link http://corner-cms.com CORNER CMS
+ */
+use panix\engine\components\Browser;
+
+class BrowserComponent extends \yii\base\Component {
+
+    private $_useragent;
+    protected $browser;
+
+    public function init() {
+
+        $this->browser = new Browser;
+        $this->_useragent = Yii::app()->request->userAgent;
+        parent::init();
+    }
+
+    public function getName($userAgent = false) {
+        $this->browser->setUserAgent(($userAgent) ? $userAgent : $this->_useragent);
+        return $this->browser->getBrowser();
+    }
+
+    public function getVersion($userAgent = false) {
+        $this->browser->setUserAgent(($userAgent) ? $userAgent : $this->_useragent);
+        return $this->browser->getVersion();
+    }
+
+    public function getPlatform($userAgent = false) {
+        $this->browser->setUserAgent(($userAgent) ? $userAgent : $this->_useragent);
+        return $this->browser->getPlatform();
+    }
+
+    public function getString($userAgent = false) {
+        $this->browser->setUserAgent(($userAgent) ? $userAgent : $this->_useragent);
+        return $this->browser->__toString();
+    }
+
+    public function isMobile($userAgent = false) {
+        $this->browser->setUserAgent(($userAgent) ? $userAgent : $this->_useragent);
+        return $this->browser->isMobile();
+    }
+
+    public function isTablet($userAgent = false) {
+        $this->browser->setUserAgent(($userAgent) ? $userAgent : $this->_useragent);
+        return $this->browser->isTablet();
+    }
+
+    public function isRobot($userAgent = false) {
+        $this->browser->setUserAgent(($userAgent) ? $userAgent : $this->_useragent);
+        return $this->browser->isRobot();
+    }
+
+    public function isFacebook($userAgent = false) {
+        $this->browser->setUserAgent(($userAgent) ? $userAgent : $this->_useragent);
+        return $this->browser->isFacebook();
+    }
+
+}
