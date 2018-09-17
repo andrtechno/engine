f01abdc5ae923557d69617f855916e9525d9748c
Andrew <andrew.panix@gmail.com>
2018-09-17 18:26:49 +0300

    init

--- bootstrap/ActiveForm.php
+++ bootstrap/ActiveForm.php
@@ -8,15 +8,19 @@ class ActiveForm extends \yii\bootstrap4\ActiveForm {
 
     public $layout = 'horizontal';
 
+    public $checkTemplate = "<div class=\"ds\"></div><div class=\"form-check\">\n{input}\n{label}\n{error}\n{hint}\n</div>";
+
+    public $checkEnclosedTemplate = "<div class=\"ds\"></div><div class=\"form-check\">\n{beginLabel}\n{input}\n{labelTitle}\n{endLabel}\n{error}\n{hint}\n</div>";
+
     public function init() {
         $this->fieldConfig = ArrayHelper::merge([
                     'template' => "{label}\n{beginWrapper}\n{input}\n{hint}\n{error}\n{endWrapper}",
                     'horizontalCssClasses' => [
-                        'label' => 'col-sm-4',
+                        'label' => 'col-sm-4 col-form-label',
                         'offset' => 'col-sm-offset-4',
                         'wrapper' => 'col-sm-8',
                         'error' => '',
-                        'hint' => ''
+                        'hint' => '',
                     ],
         ],$this->fieldConfig);
         parent::init();
