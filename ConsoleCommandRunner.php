06c8da7431c80c19cd9c2665e5a7966762fa9e16
Andrew <andrew.panix@gmail.com>
2018-09-18 19:13:04 +0300

    init

---
+++ ConsoleCommandRunner.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace panix\engine;
+class ConsoleCommandRunner
+{
+    private $outerApplication;
+    private $innerApplication;
+    private $output;
+    private $exitCode;
+
+    public function __construct($config = null)
+    {
+        if (is_null($config)) {
+            $config = '@app/config/console.php';
+            if (!is_file(\Yii::getAlias($config))) {
+                $config = '@common/config/console.php';
+            }
+            if (!is_file(\Yii::getAlias($config))) {
+                throw new \InvalidArgumentException('Config was not provided; and was not detected in common paths of basic and advanced templates');
+            }
+        }
+        if (is_string($config)) {
+            if (is_file($file = \Yii::getAlias($config))) {
+                $config = require($file);
+            } else {
+                throw new \InvalidArgumentException('if $config is a string, it should be a valid yii file path');
+            }
+        }
+        if (!is_array($config)) {
+            throw new \InvalidArgumentException('$config should either be a string (path) or array');
+        }
+        // fcgi doesn't have STDIN and STDOUT defined by default
+        defined('STDIN') or define('STDIN', fopen('php://stdin', 'r'));
+        defined('STDOUT') or define('STDOUT', fopen('php://stdout', 'w'));
+        $this->outerApplication = \Yii::$app;
+        $this->innerApplication = new \yii\console\Application($config); //this changes \Yii::$app;
+        \Yii::$app = $this->outerApplication; //we set it back
+    }
+
+    public function run($route, array $params = [])
+    {
+        $this->output = null;
+        $this->exitCode = null;
+        \Yii::$app = $this->innerApplication; //Yii::$app references to console application, while you are running your command
+        ob_start();
+        $this->exitCode = $this->innerApplication->runAction($route, $params);
+        $this->output = ob_get_clean();
+        \Yii::$app = $this->outerApplication; //now Yii::$app is outer application again (typically web application)
+        return $this;
+    }
+
+    public function getOutput()
+    {
+        return $this->output;
+    }
+
+    public function getExitCode()
+    {
+        return $this->exitCode;
+    }
+}
