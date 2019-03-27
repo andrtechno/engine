7cf3cbe2d015d6c7a014d07c3e36b44377f1ebef
Andrew <andrew.panix@gmail.com>
2019-03-27 17:59:36 +0200

    111

--- bootstrap/ActiveForm.php
+++ bootstrap/ActiveForm.php
@@ -6,28 +6,27 @@ use yii\base\InvalidCallException;
 use yii\helpers\ArrayHelper;
 use panix\engine\Html;
 
-class ActiveForm extends \yii\bootstrap4\ActiveForm {
-
+class ActiveForm extends \yii\bootstrap4\ActiveForm
+{
+    public $fieldClass = 'panix\engine\bootstrap\ActiveField';
     public $layout = 'horizontal';
     public $title;
-   // public $checkTemplate = "<div class=\"ds\"></div><div class=\"form-check\">\n{input}\n{label}\n{error}\n{hint}\n</div>";
-
-   // public $checkEnclosedTemplate = "<div class=\"ds\"></div><div class=\"form-check\">\n{beginLabel}\n{input}\n{labelTitle}\n{endLabel}\n{error}\n{hint}\n</div>";
 
     /**
      * @inheritdoc
      */
-    public function init() {
+    public function init()
+    {
         $this->fieldConfig = ArrayHelper::merge([
-                    'template' => "{label}\n{beginWrapper}\n{input}\n{hint}\n{error}\n{endWrapper}",
-                    'horizontalCssClasses' => [
-                        'label' => 'col-sm-4 col-lg-2 col-form-label',
-                        'offset' => 'col-sm-offset-4',
-                        'wrapper' => 'col-sm-8 col-lg-10',
-                        'error' => '',
-                        'hint' => '',
-                    ],
-        ],$this->fieldConfig);
+            'template' => "{label}\n{beginWrapper}\n{input}\n{hint}\n{error}\n{endWrapper}",
+            'horizontalCssClasses' => [
+                'label' => 'col-sm-4 col-lg-2 col-form-label',
+                'offset' => 'col-sm-offset-4',
+                'wrapper' => 'col-sm-8 col-lg-10',
+                'error' => '',
+                'hint' => '',
+            ],
+        ], $this->fieldConfig);
         parent::init();
     }
 
