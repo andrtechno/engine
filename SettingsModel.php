3ddeb4a5967faa6a944087d30fa1edbe49af214d
Andrew <andrew.panix@gmail.com>
2019-04-06 02:01:31 +0300

    init

--- SettingsModel.php
+++ SettingsModel.php
@@ -6,10 +6,13 @@ use Yii;
 use panix\engine\base\Model;
 use yii\base\InvalidConfigException;
 
-class SettingsModel extends Model {
+class SettingsModel extends Model
+{
     protected $category;
     const NAME = null;
-    public function init() {
+
+    public function init()
+    {
         if (!isset($this->module)) {
             throw new InvalidConfigException(Yii::t('yii', 'Missing required parameters: {params}', [
                 'params' => 'module'
@@ -21,7 +24,8 @@ class SettingsModel extends Model {
         $this->setAttributes((array)Yii::$app->settings->get($this->category));
     }
 
-    public function save() {
+    public function save()
+    {
         $shortName = (new \ReflectionClass(get_called_class()))->getShortName();
         if ($this->validate()) {
             Yii::$app->settings->set($this->category, Yii::$app->request->post($shortName));
