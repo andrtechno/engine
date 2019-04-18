1b42caee4bd6a39f91704dcc7dd72fcc50b0f860
Andrew <andrew.panix@gmail.com>
2019-04-18 13:12:56 +0300

    init

--- behaviors/NotifactionBehavior.php
+++ behaviors/NotifactionBehavior.php
@@ -3,17 +3,17 @@
 namespace panix\engine\behaviors;
 
 use Yii;
-use yii\db\ActiveRecord;
-use panix\mod\admin\models\Notifactions;
+use yii\base\Behavior;
+use panix\mod\admin\models\Notifications;
 
-class NotifactionBehavior extends \yii\base\Behavior {
+class NotifactionBehavior extends Behavior {
 
     public $type = 'info';
     public $text;
 
     public function attach($owner) {
         parent::attach($owner);
-        $notifaction = new Notifactions;
+        $notifaction = new Notifications;
         $notifaction->type = $this->type;
         $notifaction->text = $this->text;
         $notifaction->save(false);
