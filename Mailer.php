ce55fc563525b8250bdd6dbc3f4588902a9eb391
Andrew <andrew.panix@gmail.com>
2019-04-28 10:34:33 +0300

    init

--- Mailer.php
+++ Mailer.php
@@ -12,14 +12,15 @@ class Mailer extends \yii\swiftmailer\Mailer
         $config = Yii::$app->settings->get('app');
         $this->messageConfig['from'] = [$config->email => $config->sitename];
 
-        $transport['class'] = 'Swift_SmtpTransport';
-        $transport['host'] = $config->mailer_transport_smtp_host;
-        $transport['username'] = $config->mailer_transport_smtp_username;
-        $transport['password'] = $config->mailer_transport_smtp_password;
-        $transport['port'] = $config->mailer_transport_smtp_port;
-        $transport['encryption'] = $config->mailer_transport_smtp_encryption;
-        $this->setTransport($transport);
-
+        if (isset($config->mailer_transport_smtp_enabled) && $config->mailer_transport_smtp_enabled) {
+            $transport['class'] = 'Swift_SmtpTransport';
+            $transport['host'] = isset($config->mailer_transport_smtp_host) ? $config->mailer_transport_smtp_host : 'localhost';
+            $transport['username'] = $config->mailer_transport_smtp_username;
+            $transport['password'] = $config->mailer_transport_smtp_password;
+            $transport['port'] = $config->mailer_transport_smtp_port;
+            $transport['encryption'] = $config->mailer_transport_smtp_encryption;
+            $this->setTransport($transport);
+        }
         $this->getView()->renderers = Yii::$app->getView()->renderers;
         parent::init();
     }
