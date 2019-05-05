74781a59512c0973e3a38bd866160b58a2ce9a66
Andrew <andrew.panix@gmail.com>
2019-05-05 09:05:16 +0300

    init

--- data/Widget.php
+++ data/Widget.php
@@ -3,39 +3,46 @@
 namespace panix\engine\data;
 
 use Yii;
+use yii\helpers\VarDumper;
 
-class Widget extends \yii\base\Widget {
+class Widget extends \yii\base\Widget
+{
 
     public $skin = 'default';
     public $assetsUrl;
+    public $widget_id;
 
-
-    public function init() {
+    public function init()
+    {
         parent::init();
         $reflectionClass = new \ReflectionClass(get_class($this));
-        $aliasName = 'wgt_' . $reflectionClass->getShortName();
+        $this->widget_id = 'wgt_' . $reflectionClass->getShortName();
         Yii::$app->setAliases([
-            '@' . $aliasName => realpath(dirname($reflectionClass->getFileName())),
+            '@' . $this->widget_id => realpath(dirname($reflectionClass->getFileName())),
         ]);
-        $this->registerTranslations($aliasName);
 
-        if (file_exists(Yii::getAlias("@{$aliasName}/assets"))) {
-            $assetsPaths = Yii::$app->getAssetManager()->publish(Yii::getAlias("@{$aliasName}/assets"));
+
+        $this->registerTranslations($this->widget_id);
+
+        if (file_exists(Yii::getAlias("@{$this->widget_id}/assets"))) {
+            $assetsPaths = Yii::$app->getAssetManager()->publish(Yii::getAlias("@{$this->widget_id}/assets"));
             $this->assetsUrl = $assetsPaths[1];
         }
 
+        // echo VarDumper::dump(Yii::$app->i18n->translations,15,true);die;
 
     }
 
-    public function getTranslationsFileMap($id) {
+    public function getTranslationsFileMap($id)
+    {
         $lang = Yii::$app->language;
         $result = [];
         $basepath = realpath(Yii::getAlias("@{$id}/messages/{$lang}"));
         if (file_exists($basepath)) {
             if (is_dir($basepath)) {
                 $fileList = \yii\helpers\FileHelper::findFiles($basepath, [
-                            'only' => ['*.php'],
-                            'recursive' => false
+                    'only' => ['*.php'],
+                    'recursive' => false
                 ]);
                 foreach ($fileList as $path) {
                     $result[$id . '/' . basename($path, '.php')] = basename($path);
@@ -46,7 +53,8 @@ class Widget extends \yii\base\Widget {
         return $result;
     }
 
-    public function registerTranslations($id) {
+    public function registerTranslations($id)
+    {
         Yii::$app->i18n->translations[$id . '/*'] = [
             'class' => 'yii\i18n\PhpMessageSource',
             'basePath' => '@' . $id . '/messages',
@@ -54,15 +62,25 @@ class Widget extends \yii\base\Widget {
         ];
     }
 
-    public function getTitle() {
-        return basename(get_class($this));
+    public function getTitle()
+    {
+        $name = $this->getName();
+        if (file_exists(Yii::getAlias("@{$this->widget_id}/messages"))) {
+            return Yii::t("wgt_{$name}/default", 'TITLE');
+        } else {
+            return $name;
+        }
+
+
     }
 
-    public function getConfig() {
+    public function getConfig()
+    {
         return Yii::$app->settings->get($this->getName());
     }
 
-    public function getName() {
+    public function getName()
+    {
         return basename(get_class($this));
     }
 
