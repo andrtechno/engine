b24a06e96c356b4885f3226376ae8c72fa2b4a4c
Andrew <andrew.panix@gmail.com>
2020-01-24 23:37:03 +0200

    init

--- widgets/scrollTop/ScrollTop.php
+++ widgets/scrollTop/ScrollTop.php
@@ -1,9 +1,11 @@
 <?php
+
 namespace panix\engine\widgets\scrollTop;
 
 use panix\engine\data\Widget;
 
-class ScrollTop extends Widget {
+class ScrollTop extends Widget
+{
 
     public $minDepth;
     public $minHeight;
@@ -12,8 +14,8 @@ class ScrollTop extends Widget {
     public $opacity;
     public $scrollTopTime;
     public $scrollBottomTime;
-    public $enableTop=true;
-    public $enableBottom=false;
+    public $enableTop = true;
+    public $enableBottom = false;
 
 
     public function init()
@@ -35,8 +37,96 @@ class ScrollTop extends Widget {
         parent::init();
     }
 
-    public function run() {
+    public function run()
+    {
 
+        $this->getView()->registerJs("
+
+    $(document).ready(function() {
+        " . (($this->opacity > 0 && $this->opacity <= 1) ? "fadeOutTo(); showFadeOnMouseScrollButtons()" : "$(window).scroll(function(){showHideScrollButtons()});") . "
+    });
+    
+    " . (($this->opacity > 0 && $this->opacity <= 1) ? "" : "showHideScrollButtons();") . "
+
+    " . (($this->opacity > 0 && $this->opacity <= 1) ? "
+        function fadeOutTo(){
+            $('.scroll-to').fadeTo(" . $this->fadeInTime . ", " . $this->opacity . ");
+            //$('.backtobottom').fadeTo(" . $this->fadeInTime . ", " . $this->opacity . ");
+        }
+
+        function showFadeOnMouseScrollButtons(){
+            $('.scroll-to.top').mouseover(function(){
+                $('.scroll-to.top').fadeTo(" . $this->fadeInTime . ", 1);
+            });
+            $('.scroll-to.top').mouseout(function(){
+                $('.scroll-to.top').fadeTo(" . $this->fadeInTime . ", " . $this->opacity . ");
+            });
+
+            $('.scroll-to.bottom').mouseover(function(){
+                $('.scroll-to.bottom').fadeTo(" . $this->fadeInTime . ", 1);
+            });
+            $('.scroll-to.bottom').mouseout(function(){
+                $('.scroll-to.bottom').fadeTo(" . $this->fadeInTime . ", " . $this->opacity . ");
+            });
+        } 
+    " : "
+        function showHideScrollButtons(){
+            if ($(this).scrollTop() > " . $this->minHeight . ") {
+                $('.scroll-to.top').fadeIn(" . $this->fadeInTime . ");
+            } else {
+                $('.scroll-to.top').fadeOut(" . $this->fadeOutTime . ");
+            }
+
+            if (($(document).height() - $(this).scrollTop()) > " . $this->minDepth . ") {
+                $('.scroll-to.bottom').fadeIn(" . $this->fadeInTime . ");
+            } else {
+                $('.scroll-to.bottom').fadeOut(" . $this->fadeOutTime . ");
+            }
+        }
+    ") . "
+
+    
+    $('.scroll-to.top').click(function() {
+        $(\"html, body\").animate({ scrollTop: 0 }, " . $this->scrollTopTime . ");
+        return false;
+    });
+
+    $('.scroll-to.bottom').click(function() {
+        $(\"html, body\").animate({ scrollTop: $(document).height() },  " . $this->scrollBottomTime . ");
+        return false;
+    });
+
+", \yii\web\View::POS_END, 'topBottomScroll-js');
+
+        $this->getView()->registerCss(' 
+    .hide {display:none;}
+    
+    #scroll-to{
+        position: fixed;
+        bottom: 50px;
+        right: 5px;
+        opacity: 1;
+        cursor: pointer;
+        z-index:1001;
+    }
+
+    #scroll-to .scroll-to {
+        width: 42px;
+        height: 42px;
+        position:relative;
+    }
+    #scroll-to .scroll-to.top:before{
+        position:absolute;
+        font-family: "Pixelion";
+        content:"\f007";
+        width: 100%;
+        font-size: 20px;
+        left:0;
+        top: 6px;
+        text-align: center;
+        display:block;
+    }
+', [], 'topBottomScroll-css');
         return $this->render($this->skin);
     }
 
--- widgets/scrollTop/views/default.php
+++ widgets/scrollTop/views/default.php
@@ -1,105 +1,12 @@
-<div id="topcontrol">
+<div id="scroll-to">
     <?php if ($this->context->enableTop) { ?>
         <div
-        class="backtotop btn btn-secondary" <?php echo(($this->context->opacity > 0 && $this->context->opacity <= 1) ? '' : 'style="display:none;"'); ?>></div>
+        class="scroll-to top" <?php echo(($this->context->opacity > 0 && $this->context->opacity <= 1) ? '' : 'style="display:none;"'); ?>></div>
     <?php } ?>
     <?php if ($this->context->enableBottom) { ?>
         <div
-        class="backtobottom" <?php echo(($this->context->opacity > 0 && $this->context->opacity <= 1) ? '' : 'style="display:none;"'); ?>></div>
+        class="scroll-to bottom" <?php echo(($this->context->opacity > 0 && $this->context->opacity <= 1) ? '' : 'style="display:none;"'); ?>></div>
     <?php } ?>
 </div>
-<?php
-$this->registerJs("
 
-    $(document).ready(function() {
-        " . (($this->context->opacity > 0 && $this->context->opacity <= 1) ? "fadeOutTo(); showFadeOnMouseScrollButtons()" : "$(window).scroll(function(){showHideScrollButtons()});") . "
-    });
-    
-    " . (($this->context->opacity > 0 && $this->context->opacity <= 1) ? "" : "showHideScrollButtons();") . "
-
-    " . (($this->context->opacity > 0 && $this->context->opacity <= 1) ? "
-        function fadeOutTo(){
-            $('.backtotop, .btn').fadeTo(" . $this->context->fadeInTime . ", " . $this->context->opacity . ");
-            $('.backtobottom').fadeTo(" . $this->context->fadeInTime . ", " . $this->context->opacity . ");
-        }
-
-        function showFadeOnMouseScrollButtons(){
-            $('.backtotop').mouseover(function(){
-                $('.backtotop').fadeTo(" . $this->context->fadeInTime . ", 1);
-            });
-            $('.backtotop').mouseout(function(){
-                $('.backtotop').fadeTo(" . $this->context->fadeInTime . ", " . $this->context->opacity . ");
-            });
-
-            $('.backtobottom').mouseover(function(){
-                $('.backtobottom').fadeTo(" . $this->context->fadeInTime . ", 1);
-            });
-            $('.backtobottom').mouseout(function(){
-                $('.backtobottom').fadeTo(" . $this->context->fadeInTime . ", " . $this->context->opacity . ");
-            });
-        } 
-    " : "
-        function showHideScrollButtons(){
-            if ($(this).scrollTop() > " . $this->context->minHeight . ") {
-                $('.backtotop').fadeIn(" . $this->context->fadeInTime . ");
-            } else {
-                $('.backtotop').fadeOut(" . $this->context->fadeOutTime . ");
-            }
-
-            if (($(document).height() - $(this).scrollTop()) > " . $this->context->minDepth . ") {
-                $('.backtobottom').fadeIn(" . $this->context->fadeInTime . ");
-            } else {
-                $('.backtobottom').fadeOut(" . $this->context->fadeOutTime . ");
-            }
-        }
-    ") . "
-
-    
-    $('.backtotop').click(function() {
-        $(\"html, body\").animate({ scrollTop: 0 }, " . $this->context->scrollTopTime . ");
-        return false;
-    });
-
-    $('.backtobottom').click(function() {
-        $(\"html, body\").animate({ scrollTop: $(document).height() },  " . $this->context->scrollBottomTime . ");
-        return false;
-    });
-
-", \yii\web\View::POS_END, 'topBottomScroll-js');
-
-$this->registerCss(' 
-    .hide {display:none;}
-    
-    #topcontrol{
-        position: fixed;
-        bottom: 50px;
-        right: 5px;
-        opacity: 1;
-        cursor: pointer;
-        z-index:1001;
-    }
-
-    #topcontrol .btn {
-
-        width: 42px;
-        height: 42px;
-        position:relative;
-    }
-    #topcontrol .btn:before{
-        position:absolute;
-        font-family: "Pixelion";
-        content:"\f007";
-        width: 100%;
-        font-size: 20px;
-        left:0;
-        top: 6px;
-        text-align: center;
-        display:block;
-    }
-
-    .backtobottom {
-        width: 68px;
-        height: 68px;
-    }
-', [], 'topBottomScroll-css');
 
