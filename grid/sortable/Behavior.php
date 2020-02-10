e25db8920da4036315d928720aa281d5cbb80773
Andrew <andrew.panix@gmail.com>
2020-02-10 12:02:20 +0200

    init

--- grid/sortable/Behavior.php
+++ grid/sortable/Behavior.php
@@ -1,36 +1,30 @@
 <?php
+
 namespace panix\engine\grid\sortable;
+
 use Yii;
 use yii\db\ActiveRecord;
-class Behavior extends \yii\base\Behavior {
-    
+
+class Behavior extends \yii\base\Behavior
+{
+
     public $column = 'ordern';
-    
-    public function events() {
+
+    public function events()
+    {
         return [
             ActiveRecord::EVENT_BEFORE_INSERT => 'beforeSave',
 
         ];
     }
 
-
-    public function beforeFind2($event) {
-        $criteria = $this->owner->getDbCriteria();
-        $alias = $this->owner->getTableAlias(true);
-
-        if (!$criteria->order)
-            $criteria->order = $alias . ".`{$this->column}` DESC";
-        //    parent::beforeFind($event);
-    }
-
-    public function beforeSave($event) {
-
+    public function beforeSave()
+    {
+        /** @var ActiveRecord $model */
         $model = $this->owner;
         $column = $this->column;
         if ($model->isNewRecord)
             $model->$column = Yii::$app->db->createCommand("SELECT MAX({$this->column}) FROM " . $model->tableName())->queryScalar() + 1;
-        // parent::beforeSave($event);
     }
-
 
 }
