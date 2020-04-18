12baed3d7ecaed1c67ae575201bba7d34ac04315
Andrew <andrew.panix@gmail.com>
2020-04-18 17:43:56 +0300

    init

--- db/Migration.php
+++ db/Migration.php
@@ -74,6 +74,6 @@ class Migration extends \yii\db\Migration
 
     public function phone($string = 25)
     {
-        $this->string($string)->null();
+        return $this->string($string)->null();
     }
 }
