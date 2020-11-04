31a03f5af9d33e54b3a95c0cbb3038a901bcf24f
Andrew <andrew.panix@gmail.com>
2020-11-04 11:01:49 +0200

    init

--- widgets/Pjax.php
+++ widgets/Pjax.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace panix\engine\widgets;
+use yii\helpers\Json;
 
 /**
  * Class Pjax
@@ -21,36 +22,7 @@ class Pjax extends \yii\widgets\Pjax
 
     }
 
-    /*public function registerClientScript2()
-    {
 
-        $id = $this->options['id'];
-        $this->clientOptions['push'] = $this->enablePushState;
-        $this->clientOptions['replace'] = $this->enableReplaceState;
-        $this->clientOptions['timeout'] = $this->timeout;
-        $this->clientOptions['scrollTo'] = $this->scrollTo;
-        if (!isset($this->clientOptions['container'])) {
-            $this->clientOptions['container'] = "#$id";
-        }
-
-        $options = Json::htmlEncode($this->clientOptions);
-        $js = '';
-        if ($this->linkSelector !== false) {
-            $linkSelector = Json::htmlEncode($this->linkSelector !== null ? $this->linkSelector : '#' . $id . ' a');
-            $js .= "jQuery(document).pjax($linkSelector, $options);";
-        }
-        if ($this->formSelector !== false) {
-            $formSelector = Json::htmlEncode($this->formSelector !== null ? $this->formSelector : '#' . $id . ' form[data-pjax]');
-            $submitEvent = Json::htmlEncode($this->submitEvent);
-            $js .= "\njQuery(document).on($submitEvent, $formSelector, function (event) {jQuery.pjax.submit(event, $options);});";
-        }
-        $view = $this->getView();
-        PjaxAsset::register($view);
-
-        if ($js !== '') {
-            $view->registerJs($js);
-        }
-    }*/
 
     public function registerClientScript()
     {
