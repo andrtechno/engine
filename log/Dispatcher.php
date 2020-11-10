558bfcd1d4bd8e25f7129db27a77e2ed60d622f6
Andrew <andrew.panix@gmail.com>
2020-11-11 00:22:46 +0200

    init

--- log/Dispatcher.php
+++ log/Dispatcher.php
@@ -19,21 +19,23 @@ class Dispatcher extends \yii\log\Dispatcher
      */
     public function init()
     {
-
+        $config = Yii::$app->settings->get('logs');
         $date = new \DateTime(date('Y-m-d', time()), new \DateTimeZone('Europe/Kiev'));
         $date = $date->format('Y-m-d');
         $logPath = '@runtime/logs/' . $date . '/' . Yii::$app->id;
-        $this->targets = [
-            [
-                'class' => 'panix\engine\log\FileTarget',
-                'levels' => ['error', 'warning'],
-                'categories' => ['yii\db\*'],
-                'logFile' => $logPath . '/db_error.log',
-            ],
-            [
+
+
+        $this->targets[] = [
+            'class' => 'panix\engine\log\FileTarget',
+            'levels' => ['error', 'warning'],
+            'categories' => ['yii\db\*'],
+            'logFile' => $logPath . '/db_error.log',
+        ];
+        if (isset($config->query_execute)) {
+            $this->targets[] = [
                 'class' => 'panix\engine\log\FileTarget',
                 'levels' => ['info'],
-                'enabled' => YII_DEBUG,
+                'enabled' => $config->query_execute,
                 'categories' => [
                     'yii\db\Command::execute'
                 ],
@@ -48,11 +50,13 @@ class Dispatcher extends \yii\log\Dispatcher
                     'yii\web\Session::freeze',
                 ],
 
-            ],
-            [
+            ];
+        }
+        if (isset($config->query_query)) {
+            $this->targets[] = [
                 'class' => 'panix\engine\log\FileTarget',
                 'levels' => ['info'],
-                'enabled' => YII_DEBUG,
+                'enabled' => $config->query_query,
                 'categories' => [
                     'yii\db\Command::query',
                 ],
@@ -67,89 +71,89 @@ class Dispatcher extends \yii\log\Dispatcher
                     'yii\web\Session::freeze',
                 ],
 
-            ],
-
-            [
-                'class' => 'panix\engine\log\FileTarget',
-                'levels' => ['info'],
-                'enabled' => YII_DEBUG,
-                'logFile' => $logPath . '/info.log',
-                //'logVars' => [],
-                'except' => [
-                    'yii\db\Command::query',
-                    'yii\db\Command::execute',
-                    'yii\db\Connection::open',
-                    'yii\swiftmailer\Mailer::sendMessage',
-                    'yii\mail\BaseMailer::send',
-                    'yii\httpclient\StreamTransport::send',
-                    'yii\web\Session::open',
-                    'yii\web\Session::unfreeze',
-                    'yii\web\Session::freeze',
-                ],
-            ],
-            [
-                'class' => 'panix\engine\log\FileTarget',
-                'levels' => ['info'],
-                'logVars' => [],
-                'logFile' => $logPath . '/mail.log',
-                'categories' => [
-                    'yii\mail\BaseMailer::send',
-                ],
-
-            ],
-            [
-                'class' => 'panix\engine\log\FileTarget',
-                'levels' => ['profile'],
-                'enabled' => YII_DEBUG,
-                'logFile' => $logPath . '/profile.log',
-                'except' => [
-                    'yii\db\Command::query',
-                    'yii\db\Command::execute',
-                    'yii\db\Connection::open',
-                    'yii\httpclient\StreamTransport::send'
-                ],
-            ],
-            [
-                'class' => 'panix\engine\log\FileTarget',
-                'levels' => ['info'],
-                'enabled' => YII_DEBUG,
-                'logFile' => $logPath . '/httpclient.log',
-                'categories' => [
-                    'yii\httpclient\StreamTransport::send',
-                ],
-            ],
-
-
-            /*[
-                'class' => 'panix\engine\log\FileTarget',
-                'levels' => ['trace'],
-                'enabled' => false,
-                'logFile' => $logPath . '/trace.log',
-            ],*/
-
-            [
-                'class' => 'panix\engine\log\EmailTarget',
-                'levels' => ['error', 'warning'],
-                //'categories' => ['yii\base\*'],
-                'enabled' => $this->enableEmail,
-                'except' => [
-                    'yii\web\HttpException:404',
-                    'yii\web\HttpException:403',
-                    'yii\web\HttpException:400',
-                    'yii\i18n\PhpMessageSource::loadMessages'
-                ],
-            ],
-            [
-                'class' => 'panix\engine\log\FileTarget',
-                'levels' => ['error'],
-                'logFile' => $logPath . '/error.log',
-            ],
-            [
-                'class' => 'panix\engine\log\FileTarget',
-                'levels' => ['warning'],
-                'logFile' => $logPath . '/warning.log',
+            ];
+        }
+        $this->targets[] = [
+            'class' => 'panix\engine\log\FileTarget',
+            'levels' => ['info'],
+            'enabled' => YII_DEBUG,
+            'logFile' => $logPath . '/info.log',
+            //'logVars' => [],
+            'except' => [
+                'yii\db\Command::query',
+                'yii\db\Command::execute',
+                'yii\db\Connection::open',
+                'yii\swiftmailer\Mailer::sendMessage',
+                'yii\mail\BaseMailer::send',
+                'yii\httpclient\StreamTransport::send',
+                'yii\web\Session::open',
+                'yii\web\Session::unfreeze',
+                'yii\web\Session::freeze',
             ],
         ];
+        $this->targets[] = [
+            'class' => 'panix\engine\log\FileTarget',
+            'levels' => ['info'],
+            'logVars' => [],
+            'logFile' => $logPath . '/mail.log',
+            'categories' => [
+                'yii\mail\BaseMailer::send',
+            ],
+
+        ];
+        $this->targets[] = [
+            'class' => 'panix\engine\log\FileTarget',
+            'levels' => ['profile'],
+            'enabled' => YII_DEBUG,
+            'logFile' => $logPath . '/profile.log',
+            'except' => [
+                'yii\db\Command::query',
+                'yii\db\Command::execute',
+                'yii\db\Connection::open',
+                'yii\httpclient\StreamTransport::send'
+            ],
+        ];
+        $this->targets[] = [
+            'class' => 'panix\engine\log\FileTarget',
+            'levels' => ['info'],
+            'enabled' => YII_DEBUG,
+            'logFile' => $logPath . '/httpclient.log',
+            'categories' => [
+                'yii\httpclient\StreamTransport::send',
+            ],
+        ];
+
+
+        /*[
+            'class' => 'panix\engine\log\FileTarget',
+            'levels' => ['trace'],
+            'enabled' => false,
+            'logFile' => $logPath . '/trace.log',
+        ],*/
+
+        $this->targets[] = [
+            'class' => 'panix\engine\log\EmailTarget',
+            'levels' => ['error', 'warning'],
+            //'categories' => ['yii\base\*'],
+            'enabled' => $this->enableEmail,
+            'except' => [
+                'yii\web\HttpException:404',
+                'yii\web\HttpException:403',
+                'yii\web\HttpException:400',
+                'yii\i18n\PhpMessageSource::loadMessages'
+            ],
+        ];
+        $this->targets[] = [
+            'class' => 'panix\engine\log\FileTarget',
+            'levels' => ['error'],
+            'logFile' => $logPath . '/error.log',
+        ];
+        $this->targets[] = [
+            'class' => 'panix\engine\log\FileTarget',
+            'levels' => ['warning'],
+            'logFile' => $logPath . '/warning.log',
+        ];
+
         parent::init();
 
 
