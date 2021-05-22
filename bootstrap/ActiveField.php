c4e225505d5c82e29c178510b28eb86f8241c5ee
Andrew <andrew.panix@gmail.com>
2021-05-22 12:09:27 +0300

    load

--- bootstrap/ActiveField.php
+++ bootstrap/ActiveField.php
@@ -2,6 +2,8 @@
 
 namespace panix\engine\bootstrap;
 
+use yii\helpers\ArrayHelper;
+
 class ActiveField extends \yii\bootstrap4\ActiveField
 {
 
@@ -14,5 +16,70 @@ class ActiveField extends \yii\bootstrap4\ActiveField
             $options['class'] = 'custom-select w-auto';
         }
         return parent::dropdownList($items, $options);
+    }
+
+
+    /**
+     * Render Phone widget. If not found return textInput
+     * @param array $options
+     * @return $this
+     * @throws \Exception
+     */
+    public function telInput($options = [])
+    {
+        $jsOptions = ArrayHelper::remove($options, 'jsOptions', []);
+        if (class_exists('panix\\ext\\telinput\\PhoneInput')) {
+            return parent::widget(\panix\ext\telinput\PhoneInput::class, [
+                'options' => $options,
+                'jsOptions' => $jsOptions
+            ]);
+        } else {
+            return parent::textInput($options);
+        }
+    }
+
+    /**
+     * Render tinyMce widget. If not found return textarea
+     * @param array $options
+     * @return $this
+     * @throws \Exception
+     */
+    public function tinyMce($options = [])
+    {
+        $clientOptions = ArrayHelper::remove($options, 'clientOptions', []);
+        if (class_exists('panix\\ext\\tinymce\\TinyMce')) {
+            return parent::widget(\panix\ext\tinymce\TinyMce::class, [
+                'options' => $options,
+                'clientOptions' => $clientOptions
+            ]);
+        } else {
+            return parent::textarea($options);
+        }
+    }
+
+    /**
+     * Render BootstrapSelect widget. If not found return dropdownList
+     * @param array $items the option data items. The array keys are option values, and the array values
+     * are the corresponding option labels. The array can also be nested (i.e. some array values are arrays too).
+     * For each sub-array, an option group will be generated whose label is the key associated with the sub-array.
+     * If you have a list of data models, you may convert them into the format described above using
+     * [[ArrayHelper::map()]].
+     *
+     * @param array $options
+     * @return $this
+     * @throws \Exception
+     */
+    public function bootstrapSelect($items = [], $options = [])
+    {
+        $jsOptions = ArrayHelper::remove($options, 'jsOptions', []);
+        if (class_exists('panix\\ext\\bootstrapselect\\BootstrapSelect')) {
+            return parent::widget(\panix\ext\bootstrapselect\BootstrapSelect::class, [
+                'options' => $options,
+                'items' => $items,
+                'jsOptions' => $jsOptions,
+            ]);
+        } else {
+            return parent::dropdownList($items, $options);
+        }
     }
 }
