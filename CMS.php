32b8595c9313ad7ff21bf68bb3e8107f99e8af01
Andrew <andrew.panix@gmail.com>
2021-05-27 18:28:13 +0300

    Update CMS.php

--- CMS.php
+++ CMS.php
@@ -100,6 +100,8 @@ class CMS
             if ($phoneUtil->getRegionCodeForNumber($phoneNumber) == 'UA') {
                 $pattern = "/^(\+?\d{2})(\d{3})(\d{3})(\d{2})(\d{2})$/";
                 $phone = preg_replace($pattern, '($2) $3-$4-$5', $phone);
+            } else {
+                $phone = $phone2;
             }
         }
         return $phone;
