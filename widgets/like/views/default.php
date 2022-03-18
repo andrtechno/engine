6c3bf0ab3de60a3729157396b93a4a68ef16d746
Andrew <andrew.panix@gmail.com>
2022-03-19 00:04:45 +0200

    Update default.php

--- widgets/like/views/default.php
+++ widgets/like/views/default.php
@@ -1,20 +1,20 @@
 <div class="like <?= $this->context->id ?> clearfix">
-    <div class="float-left <?= $response['activeLike'] ? 'active' : ''; ?>">
-        <a class="like-up" href="/like/up/<?= $object_id; ?>"
+
+        <a class="like-up <?= $response['activeLike'] ? 'active' : ''; ?>" href="/like/up/<?= $object_id; ?>"
            title="<?= Yii::t('wgt_LikeWidget/default', 'LIKE'); ?>"
            data-toggle="tooltip"
            data-widget="<?= $this->context->id ?>"
            data-hash="<?= $this->context->hash ?>" onclick="return false;">
             <span class="count-like"><?= $response['likeCount'] ?></span>
         </a>
-    </div>
-    <div class="float-left <?= $response['activeDislike'] ? 'active' : ''; ?>">
-        <a class="like-down" href="/like/down/<?= $object_id; ?>"
+
+
+        <a class="like-down <?= $response['activeDislike'] ? 'active' : ''; ?>" href="/like/down/<?= $object_id; ?>"
            title="<?= Yii::t('wgt_LikeWidget/default', 'DISLIKE'); ?>"
            data-toggle="tooltip"
            data-widget="<?= $this->context->id ?>"
            data-hash="<?= $this->context->hash ?>" onclick="return false;">
             <span class="count-dislike"><?= $response['dislikeCount'] ?></span>
         </a>
-    </div>
+
 </div>
