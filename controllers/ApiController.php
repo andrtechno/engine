e4db568950bae6b07e48aff4aba05caa36a421cd
Andrew <andrew.panix@gmail.com>
2022-08-26 19:11:50 +0300

    Create ApiController.php

---
+++ controllers/ApiController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace panix\engine\controllers;
+
+use Yii;
+use yii\web\Controller;
+
+/**
+ * Class ApiController
+ * @package panix\engine\controllers
+ */
+class ApiController extends Controller
+{
+
+
+    public function behaviors()
+    {
+        return [
+            'access' => [
+                'class' => \panix\mod\rbac\filters\AccessControl::class,
+                'allowActions' => [
+                    '*',
+                    // The actions listed here will be allowed to everyone including guests.
+                ]
+            ],
+        ];
+    }
+
+
+    public function getAssetUrl()
+    {
+        $assetsPaths = Yii::$app->getAssetManager()->publish(Yii::getAlias("@theme/assets"));
+        return $assetsPaths[1];
+    }
+
+
+    public function actionError()
+    {
+        /**
+         * @var $handler \yii\web\ErrorHandler
+         * @var $exception \yii\web\HttpException
+         */
+        $handler = Yii::$app->errorHandler;
+        $exception = $handler->exception;
+
+        if ($exception !== null) {
+            $statusCode = $exception->statusCode;
+            $name = $exception->getName();
+            $message = $exception->getMessage();
+
+            return $this->asJson([
+                'success'=>false,
+                'status' => $statusCode,
+                'name' => $name,
+                'message' => $message
+            ]);
+        }
+    }
+
+}
