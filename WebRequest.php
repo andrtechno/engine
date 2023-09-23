69b99594240cfcb2ef19d78e61fe4fcf3081a1fa
Andrew <andrew.panix@gmail.com>
2023-09-23 23:14:26 +0300

    Update WebRequest.php

--- WebRequest.php
+++ WebRequest.php
@@ -6,40 +6,43 @@ use Yii;
 use yii\web\Request;
 use yii\web\NotFoundHttpException;
 
-class WebRequest extends Request {
+class WebRequest extends Request
+{
 
     private $_pathInfo;
 
-    public function getPathInfo() {
-        $langCode =Yii::$app->language;
+    public function getPathInfo()
+    {
+        $langCode = Yii::$app->language;
         $pathInfo = parent::getPathInfo();
         $parts = explode('/', $pathInfo);
-
-        if (in_array($parts[0], Yii::$app->languageManager->getCodes())) {
+        $langCode = ($parts[0] == 'ua') ? 'uk' : $parts[0];
+        if (in_array($langCode, Yii::$app->languageManager->getCodes())) {
             // Valid language code detected.
             // Remove it from url path to make route work and activate lang
-            $langCode = $parts[0];
+            //$langCode = $parts[0];
 
             // If language code is equal default - show 404 page
-            if ($langCode === Yii::$app->languageManager->default->slug){
+            if ($langCode === Yii::$app->languageManager->default->slug) {
                 throw new NotFoundHttpException(Yii::t('app/error', '404'));
             }
 
-           // if(Yii::$app->languageManager->default->id == Yii::$app->languageManager->active->id){
+            // if(Yii::$app->languageManager->default->id == Yii::$app->languageManager->active->id){
 
-           // }
+            // }
             //CMS::dump($parts);die;
             unset($parts[0]);
-            $pathInfo = implode( '/',$parts);
+            $pathInfo = implode('/', $parts);
         }
         //var_dump($this->baseUrl);die;
         $this->_pathInfo = $pathInfo;
+
 
         // Activate language by code
         Yii::$app->languageManager->setActive($langCode);
         return $this->_pathInfo;
     }
-	
+
     /**
      * @inheritDoc
      */
