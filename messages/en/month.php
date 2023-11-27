f103badbf184d9b9f6a62079b77c0a1110e6e67e
Andrew <andrew.panix@gmail.com>
2023-11-27 21:04:51 +0300

    Update month.php

--- messages/en/month.php
+++ messages/en/month.php
@@ -1,16 +1,16 @@
 <?php
 
 return [
-    'January' => '{n, plural, =0{January} =1{January} =2{Jan} =3{January} =4{января} =5{Jan}}',
-    'February' => '{n, plural, =0{February} =1{February} =2{Febr} =3{February} =4{February} =5{Febr}}',
-    'March' => '{n, plural, =0{March} =1{Марта} =2{March} =3{March} =4{March} =5{March}}',
-    'April' => '{n, plural, =0{April} =1{April} =2{April} =3{April} =4{April} =5{April}}',
-    'May' => '{n, plural, =0{May} =1{May} =2{May} =3{May} =4{May} =5{May}}',
-    'June' => '{n, plural, =0{June} =1{June} =2{June} =3{June} =4{June} =5{June}}',
-    'July' => '{n, plural, =0{July} =1{July} =2{July} =3{July} =4{July} =5{July}}',
-    'August' => '{n, plural, =0{August} =1{August} =2{August} =3{August} =4{August} =5{August}}',
-    'September' => '{n, plural, =0{September} =1{September} =2{September} =3{September} =4{September} =5{September}}',
-    'October' => '{n, plural, =0{October} =1{October} =2{October} =3{October} =4{October} =5{October}}',
-    'November' => '{n, plural, =0{November} =1{November} =2{November} =3{November} =4{November} =5{November}}',
-    'December' => '{n, plural, =0{December} =1{December} =2{December} =3{December} =4{December} =5{December}}',
+    'January' => '{n, plural, =0{January} =1{Jan} other{January}}',
+    'February' => '{n, plural, =0{February} =1{Feb} other{February}}',
+    'March' => '{n, plural, =0{March} =1{Mar} other{March}}',
+    'April' => '{n, plural, =0{April} =1{Apr} other{April}}',
+    'May' => '{n, plural, =0{May} =1{May} other{May}}',
+    'June' => '{n, plural, =0{June} =1{Jun} other{June}}',
+    'July' => '{n, plural, =0{July} =1{Jul} other{July}}',
+    'August' => '{n, plural, =0{August} =1{Aug} other{August}}',
+    'September' => '{n, plural, =0{September} =1{Sept} other{September}}',
+    'October' => '{n, plural, =0{October} =1{Oct} other{October}}',
+    'November' => '{n, plural, =0{November} =1{Nov} other{November}}',
+    'December' => '{n, plural, =0{December} =1{Dec} other{December}}',
 ];
